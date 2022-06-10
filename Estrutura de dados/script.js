bcd2a7e1d0d6d1ccb56e2045fed9bde3f1cd4349
Luciano Pires <lucianopires98@outlook.com>
2022-06-09 21:03:35 -0300

    estrutura de dados

---
+++ Estrutura de dados/script.js
@@ -0,0 +1,15 @@
+
+{
+    let nome = "Rodrigo";
+    let numero = 45;
+    let ano = 2022;
+    let resultado = ano - numero;
+    let quartaFeira = true;
+    
+    console.log("o nome é: " + nome)
+    console.log("o número é: " + numero);
+    console.log("o ano é: " + ano)
+    console.log(`o resultado é: ${resultado}`);
+    console.log("o tipo é: " + typeof quartaFeira);
+    console.log("eu estou usando o console.log");
+}
